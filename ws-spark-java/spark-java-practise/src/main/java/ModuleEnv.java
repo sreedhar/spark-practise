54cc9227232dce78ec198c48d9a5820f055efbbf
Sreedhar Nagabhairava <sreedhar2009@gmail.com>
2016-05-19 18:07:53 -0400

    Added ModuleEnv to practise module

---
+++ ws-spark-java/spark-java-practise/src/main/java/ModuleEnv.java
@@ -0,0 +1,9 @@
+/**
+ * Define the constants for Paths etc to be used in code
+ */
+public class ModuleEnv {
+    public static String SPARK_PRACTISE_HOME="<Put absolute path here>\\spark-practise";
+    public static String DATA_ROOT= SPARK_PRACTISE_HOME+ "\\ws-data";
+    public static String FILE_RATINGS = ModuleEnv.DATA_ROOT + "\\ml-latest-small\\ratings.csv";
+    public static String FILE_MOVIES = ModuleEnv.DATA_ROOT + "\\ml-latest-small\\movies.csv";
+}
